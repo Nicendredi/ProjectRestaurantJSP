6ca43317da4ff068796e5d5a645d6552df45caa8
clement code <mrdominiqueclmeent@gmail.com>
2022-05-17 17:01:27 +0200

    fin preparation bdd

--- src/dao/DaoCommandesMySql.java
+++ src/dao/DaoCommandesMySql.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -47,15 +48,41 @@ public class DaoCommandesMySql implements DaoCommandes {
 	public List<Commande> findByClient(Client client) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 
-ArrayList<Client> client = new ArrayList<Client>() 
+		ArrayList<Commande> cli = new ArrayList<Commande>();
+		Connection conn = ConnectionMySql.getInstance().getConnection();
+		String sql = "select * from commandes where client='" + client + "'";
 
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery(sql);
 
-		return null;
+		while (rs.next()) {
+			Commande icm = new CommandeV1();
+			icm.setIdClient(rs.getInt("idclient"));
+			icm.setIdCommande(rs.getInt("idcommande"));
+			icm.setTotal(rs.getInt("total"));
+			icm.setInfo(rs.getString("info"));
+			cli.add(icm);
+		}
+
+		return cli;
 	}
 
 	@Override
 	public void create(Commande commande) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
+
+		Connection conn = ConnectionMySql.getInstance().getConnection();
+
+		String sql = "insert into personnes values (?,?,?,?)";
+
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setInt(1, commande.getIdCommande());
+		ps.setInt(2, commande.getIdClient());
+		ps.setInt(3, commande.getTotal());
+		ps.setString(4, commande.getInfo());
+		ps.executeUpdate();
+
+		conn.close();
 
 	}
 
